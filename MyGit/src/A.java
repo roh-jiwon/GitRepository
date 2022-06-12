81be8eeadb7215cb51d40f349bc8bb267ae19581
노지원 <@DESKTOP-4P75CIQ>
2022-06-12 21:37:56 +0900

    third change

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,5 +4,6 @@ public class A {
 		System.out.println("Hello");
 		System.out.println("This is A class");
 		System.out.println("Plus any line");
+		System.out.println("Master branch change");
 	}
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -3,6 +3,5 @@ public class B {
 	public static void main(String args[]) {
 		System.out.println("Hello");
 		System.out.println("This is B class");
-		System.out.println("Plus any other line");
 	}
 }
