e302d712cfa8d8739f6835b06aa9940b619ca78e
노지원 <@DESKTOP-4P75CIQ>
2022-06-12 21:22:43 +0900

    first change

--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -3,5 +3,6 @@ public class B {
 	public static void main(String args[]) {
 		System.out.println("Hello");
 		System.out.println("This is B class");
+		System.out.println("Plus any other line");
 	}
 }
