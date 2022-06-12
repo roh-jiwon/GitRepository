749369526163e37c4195d3ae60293fcf9fcb5c90
노지원 <@DESKTOP-4P75CIQ>
2022-06-12 21:35:54 +0900

    second change

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,6 +3,6 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello");
 		System.out.println("This is A class");
-		System.out.println("Plus any line");
+		System.out.println("A class to devC branch");
 	}
 }
